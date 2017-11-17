bd17c7583c17fac753e60766f69da1ca21356fd3
guilleben <guillermobenasulin@gmail.com>
2017-11-17 09:31:13 -0200

    first commit

---
+++ src/Calculadora.java
@@ -0,0 +1,18 @@
+
+public class Calculadora {
+	
+	private int valor = 0;
+
+	public int Valor() {
+		// TODO Auto-generated method stub
+		return valor;
+	}
+
+	public void Sumar(int PrimerSumando, int SegundoSumando) {
+		// TODO Auto-generated method stub
+	valor = PrimerSumando+SegundoSumando;
+		
+	}
+	
+
+}
---
+++ src/TestBasicos.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestBasicos {
+	
+	private Calculadora calc;
+
+	@Before
+	public void setUp() throws Exception {
+		calc = new Calculadora();
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void Una_nueva_calculadora_debe_iniciar_en_cero() {
+		//Calculadora calc = new Calculadora();
+		Assert.assertEquals(0, calc.Valor());
+			
+	}
+	
+	@Test
+	public void Sumar_2_mas_3_debe_dar_5() {
+		//Calculadora calc = new Calculadora();
+		
+		calc.Sumar(2,3);
+		Assert.assertEquals(5, calc.Valor());
+			
+	}
+	
+	@Test
+	public void Sumar_4_mas_3_debe_dar_7() {
+		//Calculadora calc = new Calculadora();
+		
+		calc.Sumar(4,3);
+		Assert.assertEquals(7, calc.Valor());
+			
+	}
+
+}
